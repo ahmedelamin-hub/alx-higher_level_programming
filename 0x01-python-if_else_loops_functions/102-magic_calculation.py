9644fd237580503b74183934f6be102a0127567d
root <ahmedelamin42@gmail.com>
2024-01-03 15:26:29 -0800

    16

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 # function for mag calc
-def magic_calculation(a,b,c):
+def magic_calculation(a, b, c):
     if a < b:
         return (c)
     if c > b:
         return (a+b)
     return (a*b - c)
-    
