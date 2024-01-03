c88744ea076161b0db71e611caaf82c854c78342
root <ahmedelamin42@gmail.com>
2024-01-03 15:32:10 -0800

    16

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,8 +5,7 @@ def fizzbuzz():
             print("FizzBuzz ", end="")
         elif n % 3 == 0:
             print("Fizz ", end="")
-        elif n %5 == 0:
+        elif n % 5 == 0:
             print("Buzz ", end="")
         else:
             print("{} ".format(n), end="")
-
