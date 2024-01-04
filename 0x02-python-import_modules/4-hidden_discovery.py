ed1d55e34495e26ffea15f32a40fc04d410a3751
root <ahmedelamin42@gmail.com>
2024-01-04 11:24:55 -0800

    new

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == " __main__":
     from hidden_4 import *
-    allf = dir()
-    for x in range(0, len(allf)):
-        if allf[x][:2] != "__":
-            print("{:s}".format(allf[x]))
+
+    fl = dir()
+    for x in range(0, len(fl)):
+        if fl[x][:2] != "__":
+            print("{:s}".format(fl[x]))
