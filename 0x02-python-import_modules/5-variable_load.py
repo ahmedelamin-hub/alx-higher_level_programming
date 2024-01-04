34cde5899d35415c7f4474c0dbefd88608f96484
root <ahmedelamin42@gmail.com>
2024-01-04 11:37:38 -0800

    new

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-
 if __name__ == "__main__":
-    print(a)
-def find_var():
-    print(a)
-if __name__ == "__main__":
-    find_var()
+    from variable_load_5 import a
 
+    print(f"{a}")
