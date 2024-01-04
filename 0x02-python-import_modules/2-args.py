187c7061b4398561e1b1ec8be4afac79854f0739
root <ahmedelamin42@gmail.com>
2024-01-04 10:15:57 -0800

    g

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 from sys import argv
-if __name__ == "__main__":
-    ars = "arguments" if argc != 1 else "argument"
-    argc = len(argv) - 1
-    print("{} {}{}{}".format(argc, args, ":" if argc != 0 else ",", "")
 
-            for i in range(1, argc + 1):
-            print ("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    a = len(argv) - 1
+    args = "arguments" if a != 1 else "argument"
+    print("{} {}{}{}".format(a, args, ":" if a != 0 else ".", ""))
+
+    for i in range(1, a + 1):
+        print("{}: {}".format(i, argv[i]))
