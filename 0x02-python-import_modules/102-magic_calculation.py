b6902f96579992e8f26cbe5d08eb4f156fc41b8f
root <ahmedelamin42@gmail.com>
2024-01-04 13:09:42 -0800

    g

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,6 +6,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for we in range(4, 6):
             c = add(c, we)
-            return c
-        else:
-            return sub(a, b)
+        return c
+    else:
+        return sub(a, b)
