bebc559bf8178e68f6a94b26a8dbed94705b8583
root <ahmedelamin42@gmail.com>
2024-01-04 11:51:14 -0800

    g

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a, operator, b = int(argv[1]), argv[2], int(argv[3])
+
+    if operator == "+":
+        result = a + b
+    elif operator == "-":
+        result = a - b
+    elif operator == "*":
+        result = a * b
+    elif operator == "/":
+        result = a / b
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
+    exit(0)
