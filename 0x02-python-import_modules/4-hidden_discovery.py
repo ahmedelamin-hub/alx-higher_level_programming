0f4b7a7b40bee658762f47ada24bb74fb008ef35
root <ahmedelamin42@gmail.com>
2024-01-04 11:22:14 -0800

    new

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == " __main__":
     from hidden_4 import *
-    tobe = dir()
-    for x in range(0, len(tobe)):
-        if tobe[x][:2] != "__":
-            print("{:s}".format(tobe[x]))
+    allf = dir()
+    for x in range(0, len(allf)):
+        if allf[x][:2] != "__":
+            print("{:s}".format(allf[x]))
