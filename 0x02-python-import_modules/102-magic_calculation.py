cf9e05a554501be1095c453db1b437b168f0975c
root <ahmedelamin42@gmail.com>
2024-01-04 12:52:06 -0800

    g

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for we in range(4, 6):
+            c = add(c, we)
+            return c
+        else:
+            return sub(a, b)
