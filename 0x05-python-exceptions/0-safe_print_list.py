8158d4d7ae944e6c69212037fceafaf17ee4d4b4
root <ahmedelamin42@gmail.com>
2024-01-22 11:22:35 -0800

    L

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    t = 0
     try:
-        for i in range(x):
-            print(my_list[i], end='')
-            count += 1
+        for the in range(x):
+            print(my_list[the], end='')
+            t += 1
     except IndexError:
         pass
     print()  # To ensure a new line is printed after the list elements
-    return count
+    return t
