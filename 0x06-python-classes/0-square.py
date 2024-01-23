5c8a899a5a7dd1085e2e46b89443d3b0fc4b2dc8
root <ahmedelamin42@gmail.com>
2024-01-23 09:27:56 -0800

    j

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class.
+Its purpose is to provide a simple representation of a square.
+"""
+
+class Square:
+    """
+    This class represents a square.
+    It's an empty class with no attributes or methods, used as a demonstration.
+    """
+    pass
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
