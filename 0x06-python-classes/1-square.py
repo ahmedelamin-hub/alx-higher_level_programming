9a07e503c623f7eb31a561b002bc01f767f60047
root <ahmedelamin42@gmail.com>
2024-01-23 09:30:20 -0800

    j

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class.
+It represents a square with a private size attribute.
+"""
+
+class Square:
+    """
+    Represents a square with a private instance attribute: size.
+
+    Attributes:
+    size (int): The size of the square, not directly accessible.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+        size (int): The size of the square.
+        """
+        self.__size = size
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
