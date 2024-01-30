1ba08929cc77f83ad51db84797ab21ce64bbe1b8
root <ahmedelamin42@gmail.com>
2024-01-30 12:17:40 -0800

    l

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+'''defines class'''
+
 
 def magic_string():
+    '''magic string'''
+    
     magic_string.counter = getattr(magic_string, 'counter', 0) + 1
     return "BestSchool, " * (magic_string.counter - 1) + "BestSchool"
