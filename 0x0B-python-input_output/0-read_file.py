f082f0d3bcdbeef461ca9122799ca170d2209892
root <ahmedelamin42@gmail.com>
2024-02-06 08:32:12 -0800

    j

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module provides a function to read and print text
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints its contents to stdout.
+
+    Args:
+    filename (str): The name of the file to read.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read())
