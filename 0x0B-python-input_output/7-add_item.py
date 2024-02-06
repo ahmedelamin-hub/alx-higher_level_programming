ab81aa1ac739ba8c93c380a85f62b5c3a0630170
root <ahmedelamin42@gmail.com>
2024-02-06 10:13:44 -0800

    j

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
-
-
+"""function that adds args to python json."""
 import sys
-
-def append_new_items(file_name, new_items):
-    """Appends new items to the list stored in the specified file."""
-    current_items = load_list_from_file(file_name)
-    current_items.extend(new_items)
-    save_to_json_file(current_items, file_name)
-
-def load_list_from_file(file_name):
-    """Loads a list from a file, returning an empty list if the file doesn't exist."""
-    try:
-        return load_from_json_file(file_name)
-    except FileNotFoundError:
-        return []
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-    new_items = sys.argv[1:]
-    append_new_items("add_item.json", new_items)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""functiion that changes to json python."""
+
+
+def class_to_json(obj):
+    """Returns the dict of function."""
+    return obj.__dict__
