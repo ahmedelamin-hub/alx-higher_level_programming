c34dd690db070553019308a8bb85ce0cffea9d56
root <ahmedelamin42@gmail.com>
2024-02-13 10:32:36 -0800

    J

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Module for Rectangle class."""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A class representing a rectangle that inherits Base."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle
+            x (int, optional): The x coordinate
+            y (int, optional): The y coordinate
+            id (int, optional): The id of the object
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Get the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        # Validation can be added here
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        # Validation can be added here
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get the x coordinate of the rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        # Validation can be added here
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get the y coordinate of the rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        # Validation can be added here
+        self.__y = value
