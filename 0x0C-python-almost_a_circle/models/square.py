4b1199cbe521a3d7e8befc2f99dbc3f4d17cb55c
root <ahmedelamin42@gmail.com>
2024-02-15 09:08:27 -0800

    L

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,6 +3,7 @@
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """Square class that inherits from Rectangle."""
 
@@ -10,6 +11,36 @@ class Square(Rectangle):
         """Initialize a Square instance."""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Get or set the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return the string representation of the Square."""
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+    def update(self, *args, **kwargs):
+        """Update the Square's attributes."""
+        attrs = ['id', 'size', 'x', 'y']
+        if args:
+            for attr, value in zip(attrs, args):
+                if attr == 'size':
+                    self.size = value
+                else:
+                    setattr(self, attr, value)
+        else:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    self.size = value
+                else:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of a Square."""
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
