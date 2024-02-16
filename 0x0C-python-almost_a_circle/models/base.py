a4f272d5d8342fe433a919b6df49e53134bc6a09
root <ahmedelamin42@gmail.com>
2024-02-16 11:57:17 -0800

    K

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,3 +25,43 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation"""
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation."""
+        filename = cls.__name__ + ".json"
+        list_dicts = [obj.to_dictionary() for obj in list_objs] if list_objs else []
+        with open(filename, 'w') as file:
+            file.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the list represented by json_string."""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes set."""
+        dummy = cls(1, 1) if cls.__name__ == "Rectangle" else cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances from a file."""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r') as file:
+                list_dicts = cls.from_json_string(file.read())
+            return [cls.create(**d) for d in list_dicts]
+        except FileNotFoundError:
+            return []
