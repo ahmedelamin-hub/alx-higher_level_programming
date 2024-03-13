68ee2f4ac843920faa9fef26b7dbabaf5ba7d56e
root <ahmedelamin42@gmail.com>
2024-03-13 13:44:36 -0700

    g

---
+++ 0x0E-SQL_more_queries/1-create_usr.sql
@@ -0,0 +1,2 @@
+-- Attempt to grant privileges to the user, implicitly creating them if they do not exist.
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' WITH GRANT OPTION;
